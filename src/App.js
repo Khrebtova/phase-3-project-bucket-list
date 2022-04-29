e3b5724e8717a4e1a8e494ba0617c9762f116b46
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-04-29 15:37:53 -0700

    add dependencies for styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import { Button } from '@mui/material';
 
-function App() {
+// const useStyles  = makeStyles({
+//   button: {backgroundColor: 'red'}
+// });
+
+const App = () => {  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world! it will be your bucket list app</h1>
+      
     </div>
   );
 }
