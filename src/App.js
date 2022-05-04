c2525e88eb20fbf5d4e56439962f2f290b0e947d
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-05-04 10:49:16 -0700

    add useEffect function to App to send GET request and store data in state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-Import * as React from 'react';
+import * as React from 'react';
+
 import Header from './components/Header';
 import Homepage from './components/Homepage';
 import NewItemForm from './components/NewItemForm';
+import { dataURL } from './Global';
 
 const App = () => {  
+  const [list, setList] = React.useState([]);
+  const [displayList, setDisplayList] = React.useState([]);
+  const [allCategories, setAllCategories] = React.useState([]);
+  
+  React.useEffect(() => {
+    document.title = "Bucket List | Home";
+    
+    fetch(dataURL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+
+    fetch(dataURL+`/categories`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+  }, []);
 
   return (
     <div className="App">
